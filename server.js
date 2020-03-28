72e1e93e97237c243529e200e4f56f77d3d7f660
Merakli, Ammar (ITCDEC) - KLM <ammar.merakli@klm.com>
2020-03-28 19:38:00 +0100

    database connection

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 // Connect Database
 connectDB();
 
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(cors());
 
 // Define Routes
-app.use('/api/todos', require('./routes/api/todos'));
+app.use("/api/todos", require("./routes/api/todos"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/dist'));
+  app.use(express.static("client/dist"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
